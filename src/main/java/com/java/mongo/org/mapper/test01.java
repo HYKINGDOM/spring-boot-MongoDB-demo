0b8837ed371ab14d94aa5e1380604b495d72fa41
yihurCoder <1370054591@qq.com>
2022-11-08 20:38:31 +0800

    mongodb transaction

---
+++ src/main/java/com/java/mongo/org/mapper/test01.java
@@ -0,0 +1,9 @@
+package com.java.mongo.org.mapper;
+
+public abstract class test01 {
+
+
+    void demo01() {
+        System.out.println();
+    }
+}
---
+++ src/main/java/com/java/mongo/org/mapper/test02.java
@@ -0,0 +1,9 @@
+package com.java.mongo.org.mapper;
+
+public class test02 extends test01 {
+
+
+    public void demo02() {
+        demo01();
+    }
+}
